6e073b968faddd746ce05e0331ed6a7f78e5b178
Benjie Harthun <bharthun7@gmail.com>
2026-02-18 22:07:02 -0600

    Reorder columns in datasets for ease of use

--- hof-model.py
+++ hof-model.py
@@ -1,8 +1,8 @@
 import pandas as pd
 
-#get datasets for eligible and ineligible players from saved csv files
-eligible=pd.read_csv("eligible_player_data.csv")
-ineligible=pd.read_csv("ineligible_player_data.csv")
+# get datasets for eligible and ineligible players from saved csv files
+eligible = pd.read_csv("eligible_player_data.csv")
+ineligible = pd.read_csv("ineligible_player_data.csv")
 
 # minor corrections needed for players with incorrect HOF status
 hof_additions = {
@@ -37,9 +37,59 @@ hof_additions = {
     "Paul Westphal",
 }
 hof_removals = {"Reggie Hanson", "Nate Johnston", "Red Dehnert", "John Thompson"}
-eligible.loc[eligible["full_name"].isin(hof_additions),"Hall of Fame Inductee"]=1
-eligible.loc[eligible["full_name"].isin(hof_removals),"Hall of Fame Inductee"]=0
+eligible.loc[eligible["full_name"].isin(hof_additions), "Hall of Fame Inductee"] = 1
+eligible.loc[eligible["full_name"].isin(hof_removals), "Hall of Fame Inductee"] = 0
 
-#fix Paul Millsap having two 2nd Team All-Rookie awards on nba.com
-ineligible.loc[ineligible["full_name"]=="Paul Millsap","2nd Team All-Rookie Team"]=1
-print(eligible[eligible["full_name"]=="Tim Duncan"].iloc[0].to_dict())
+# fix Paul Millsap having two 2nd Team All-Rookie awards on nba.com
+ineligible.loc[
+    ineligible["full_name"] == "Paul Millsap", "2nd Team All-Rookie Team"
+] = 1
+
+# reorder columns in a more logical ordering than alphabetical order
+stat_order = [
+    "GP",
+    "GS",
+    "MIN",
+    "FGM",
+    "FGA",
+    "FG_PCT",
+    "FG3M",
+    "FG3A",
+    "FG3_PCT",
+    "FTM",
+    "FTA",
+    "FT_PCT",
+    "OREB",
+    "DREB",
+    "REB",
+    "AST",
+    "STL",
+    "BLK",
+    "TOV",
+    "PF",
+    "PTS",
+    "MPG",
+    "FGMPG",
+    "FGAPG",
+    "FG3MPG",
+    "FG3APG",
+    "FTMPG",
+    "FTAPG",
+    "ORPG",
+    "DRPG",
+    "RPG",
+    "APG",
+    "SPG",
+    "BPG",
+    "TPG",
+    "FPG",
+    "PPG",
+]
+stat_order += ["PF_" + i for i in stat_order]
+eligible = pd.concat(
+    [eligible.iloc[:, :5], eligible.loc[:, stat_order], eligible.iloc[:, 79:]], axis=1
+)
+ineligible = pd.concat(
+    [ineligible.iloc[:, :5], ineligible.loc[:, stat_order], ineligible.iloc[:, 79:]],
+    axis=1,
+)
